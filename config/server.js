cbf0e8145d4715eef0845a846ffd4c85f84f69c7
abiezer <areyes@voiceteam.com>
2023-10-09 12:00:15 -0400

    after migration

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
-module.exports = ( { env } ) => ( {
-  host: env( 'HOST', '0.0.0.0' ),
-  port: env.int( 'PORT', 1337 ),
-  admin: {
-    auth: {
-      secret: env( 'ADMIN_JWT_SECRET', 'af92d5974b48f51f3e4f225395351190' ),
-    },
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
   },
-} );
+});
